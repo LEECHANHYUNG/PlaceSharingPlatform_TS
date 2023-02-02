39a596888017657aae5f43b9c91e6ef764de746a
LEECHANHYUNG <lsj721538@gmail.com>
2023-02-02 17:20:45 +0900

    feat: add main-page branch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import { Fragment } from 'react';
-import MainHeader from '../components/layout/MainHeader';
 import GlobalStyle from '../components/styled/globalStyled';
+import MainHeader from '../components/layout/MainHeader';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
